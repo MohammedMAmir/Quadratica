4830ac26ebc1c985fcb8a355a0409ba7c4b05cb4
Mohammed <muji-amir@hotmail.com>
2024-07-05 07:56:42 -0400

    added prettier config for linting and formatted all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-// Astro is configured using astro.config.mjs. 
+// Astro is configured using astro.config.mjs.
 //This file is optional if you do not need to configure Astro, but you may wish to create it now.
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({});
